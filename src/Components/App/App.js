fe6bb5cf69928f74142e4bc5c470485f202cf27e
James <jkeezer@pm.me>
2023-04-05 18:44:37 -0400

    App searchResults prop - passing to TrackList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
 import { SearchBar } from "../SearchBar/SearchBar";
+import { SearchResults } from "../SearchResults/SearchResults";
+import { PlayList } from "../Playlist/Playlist";
 
 class App extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.state.searchResults = { 
+      name: 'name',
+      artist: 'artist',
+      album: 'album',
+      id: 'id' 
+    };
+  }
   render() {
     return (
       <div>
@@ -12,7 +23,7 @@ class App extends React.Component() {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            (Add a SearchResults component) (Add a Playlist component)
+            <SearchResults searchResults={this.state.searchResults} /> <PlayList />
           </div>
         </div>
       </div>
