ddd1019d4216a1da2cd7a934e7396062a115fe8c
James <jkeezer@pm.me>
2023-04-09 23:19:28 -0400

    addTrack in Track component complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     } else {
 
       tracks.push(track);
-      this.setState({ searchResults: tracks });
+      this.setState({ playlistTracks: tracks });
 
     }
   }
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,15 +9,18 @@ class Track extends React.Component {
   }
   renderAction() {
       if (this.props.isRemoval) {
-        return <button className="Track-action" onClick={this.addTrack}>-</button>
+        return <button className="Track-action">-</button>
       } else {
         return <button className="Track-action" onClick={this.addTrack}>+</button>
       }
   }
 
+
   addTrack() {
-    this.props.onAdd = this.props.track;
+    const track = this.props.track;
+    this.props.onAdd(track);
   }
+
 
   render() {
     return (
