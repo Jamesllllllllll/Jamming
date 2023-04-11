63482373b98065ab1108d247a208c4c50d296a89
James <jkeezer@pm.me>
2023-04-10 22:49:58 -0400

    savePlaylist() complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -45,7 +45,13 @@ class App extends React.Component {
   }
 
   savePlaylist() {
-    // const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({
+        playlistName: "New Playlist",
+        playlistTracks: [],
+      });
+    });
   }
 
   search(term) {
