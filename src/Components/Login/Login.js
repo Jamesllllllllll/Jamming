389b4a448e3107a33f47113055e923ed73aaf15c
James <jkeezer@pm.me>
2023-04-15 16:39:36 -0400

    Added font-awesome Spoify icon. Phew.

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 import "./Login.css";
 
 function Login(props) {
     return (
         <div className="login">
-        <button onClick={props.onClick}>Login to Spotify</button>
+        <button onClick={props.onClick}>Login to Spotify <FontAwesomeIcon icon={icon({name: 'spotify', style: 'brands'})} /></button>
         </div>
     )
 }
